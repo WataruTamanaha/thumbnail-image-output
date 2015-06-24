804e9581e858eb74bc7d5e408f5233a3ec0b1aa9
Wataru Tamanaha <wataru@wxy.jp>
2015-06-24 23:25:20 +0900

    修正

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 <?php
 // コンテンツ代入（テスト）
-$text = '<img><iframe width="560" height="315" src="https://www.youtube.com/embed/Ue4PCI0NamI" frameborder="0" allowfullscreen></iframe><iframe src="https://player.vimeo.com/video/65018379" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><img src="http://lorempixel.com/output/sports-q-c-640-480-4.jpg" alt="" width="640"><img style="-webkit-user-select: none" src="http://lorempixel.com/output/abstract-q-c-640-480-8.jpg">';
+$text = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ue4PCI0NamI" frameborder="0" allowfullscreen></iframe><iframe src="https://player.vimeo.com/video/65018379" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><img src="http://lorempixel.com/output/sports-q-c-640-480-4.jpg" alt="" width="640"><img style="-webkit-user-select: none" src="http://lorempixel.com/output/abstract-q-c-640-480-8.jpg">';
 
 if(stristr($text, 'youtube')) { // Youtube動画を取得
 	preg_match('/youtube\.com\/embed\/(\w+)\"/i',$text,$match);
@@ -15,7 +15,6 @@ if(stristr($text, 'youtube')) { // Youtube動画を取得
 }
 elseif(stristr($text, 'vimeo')) { // Vimeo動画を取得
 	preg_match('/vimeo\.com\/video\/(\w+)\"/',$text,$match);
-	$imgid = 6271487;
 	$hash = unserialize(file_get_contents('http://vimeo.com/api/v2/video/'.$match[1].'.php'));
 	$imgsrc =  $hash[0]['thumbnail_large'];
 	echo '<img src="'.$imgsrc.'" />';
